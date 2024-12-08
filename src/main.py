2d386c5e974adaca32f800852a89843d17192982
RyKaT07 <patrykski07@gmail.com>
2024-12-08 05:53:17 +0100

    feat: logging

--- src/main.py
+++ src/main.py
@@ -5,6 +5,9 @@ from board import D27, D17, D0, D5, D6, D13, D19, D26
 import time
 from mfrc522 import SimpleMFRC522
 import adafruit_character_lcd.character_lcd as character_lcd
+import logging
+
+
 
 Darek = "192463859248"
 nieznany_uzytkownik = "86474552968"
@@ -38,17 +41,29 @@ def WaitForTag():
 if __name__=="__main__":
     # Inicjalizcja wyswietlacza
     lcd = character_lcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows)
-    lcd.message = "  Menti Sense\nOdruch zostanie"
+    lcd.message = "  Menti Sense || Odruch zostanie \n Ladowanie" 
+
+    logging.basicConfig(
+    filename='service.log',   # Plik do logowania
+    level=logging.INFO,       # Poziom logowania (INFO, DEBUG, ERROR itp.)
+    format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+    logging.info("Zaincjalizowano wyświetlacz.")
 
     # Inicjalizacja buzzera
     buzzer = digitalio.DigitalInOut(D17)
     buzzer.direction = digitalio.Direction.OUTPUT
 
+    logging.info("Zaincjalizowano buzzer")
     # Inicjalizacja czujnika temperatury
     dht_device = adafruit_dht.DHT11(D27)
 
+    logging.info("Zaincjalizowano czujnik temperatury")
+    lcd.message = "  Menti Sense || Odruch zostanie \n Przyloz karte" 
     tag = WaitForTag()
+    logging.info("Próba autoryzacji.")
     if tag in available_tags:
+        logging.info("Znaleziono uytkownika")
         lcd.clear()
         lcd.message = "  Witaj Darku \nJak ci minął dzień?"
         zalogowany = True
@@ -57,6 +72,7 @@ if __name__=="__main__":
             try:
                 print("{}, {}".format(dht_device.temperature, dht_device.humidity))
                 lcd.message="T={}\nH={}".format(dht_device.temperature, dht_device.humidity)
+                logging.info("Zmierzona temperatura {}, {}".format(dht_device.temperature, dht_device.humidity))
             except:
                 print("Error in read")
             time.sleep(0.5)
